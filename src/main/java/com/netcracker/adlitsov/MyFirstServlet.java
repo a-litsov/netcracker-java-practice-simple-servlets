dee7d3a8e7fc3b9df217fd50ae956b04b0f7633d
alex <adlitsov@gmail.com>
2018-05-11 02:16:29 +0300

    Modified to use POST request method instead of GET.

--- src/main/java/com/netcracker/adlitsov/MyFirstServlet.java
+++ src/main/java/com/netcracker/adlitsov/MyFirstServlet.java
@@ -8,7 +8,7 @@ import java.io.IOException;
 
 public class MyFirstServlet extends HttpServlet {
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         String login = req.getParameter("login");
         String password = req.getParameter("password");
 
